36b2e44513ad757f22b54f23bd8d1abdd146300e
Simon <yuanxiaolongwan@autohome.com.cn>
2017-03-01 21:15:57 +0800

    新增自定义文件

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -5,15 +5,16 @@
  * @date        2017-03-01
  * -------------------------------
  */
-const gulp         = require('gulp');
-const sass         = require('gulp-ruby-sass'); // sass编译
+const gulp = require('gulp');
+const sass = require('gulp-ruby-sass'); // sass编译
 const autoprefixer = require('gulp-autoprefixer'); // 自动不起css3前缀
-const size         = require('gulp-size'); // 计算文件大小
-const sourcemaps   = require('gulp-sourcemaps'); //配置sourcemaps文件功能
-const header       = require('gulp-header'); //添加文件头信息
-const gutil        = require('gulp-util'); //打印日志，获取参数变量等
-const cached       = require('gulp-cached'); // 缓存当前任务中的文件，只让已修改的文件通过管道
-const notify       = require('gulp-notify'); //通知
+const size = require('gulp-size'); // 计算文件大小
+const sourcemaps = require('gulp-sourcemaps'); //配置sourcemaps文件功能
+const header = require('gulp-header'); //添加文件头信息
+const gutil = require('gulp-util'); //打印日志，获取参数变量等
+const cached = require('gulp-cached'); // 缓存当前任务中的文件，只让已修改的文件通过管道
+const notify = require('gulp-notify'); //通知
+const prompt = require('prompt');
 
 /**
  * @function
@@ -21,40 +22,62 @@ const notify       = require('gulp-notify'); //通知
  * @param {object} CONFIG 基础配置参数对象
  * @param {object} browserSync 异步浏览器控制
  */
-module.exports = function(PATHS, CONFIG, browserSync){
-  // 获取package.json对象
-  const PKG = require(PATHS.ROOT+'package.json');
+module.exports = function (PATHS, CONFIG, browserSync) {
+	// 获取package.json对象
+	const PKG = require(PATHS.ROOT + 'package.json');
 
-  // sass编译
-  return sass(PATHS.BEFORE.CSS + '**/**/*.scss',{
-          sourcemap: false,
-          precision: 6,           // sass中计算精度
-          // stopOnError: true,   // 错误是否忽略继续编译
-          style: "compressed",    // 压缩css
-          emitCompileError:true,
-          loadPath:['./'+PATHS.BEFORE.CSS+'core', './'+PATHS.BEFORE.CSS+'module'] //查找文件根目录
-      })
-      .on('error', function(err) { // 打印日志
-        notify.onError({
-            title: 'Error!',
-            // subtitle: 'fail!',
-            message: '<%= error.message %>',
-            sound: 'Beep'
-        })(err);
-        this.emit('end');
-      })
-      .pipe(cached('sass'))
-      .pipe(autoprefixer({
-          browsers: ['ios >= 6', 'android >= 4.0']
-      }))
-      .pipe(sourcemaps.write())
-      .pipe(header(PKG.banner,{pkg: PKG}))
-      .pipe(size({title: 'styles',gzip: true}))
-      .pipe(gulp.dest(PATHS.AFTER.CSS))
-      .pipe(browserSync.stream())
-      .pipe(notify({ // 编译完成
-           title: 'SASS',
-           subtitle: 'success!',
-           message: 'Sass task complete！'
-       }));
+	// sass编译
+	var compile = (file) => {
+		return sass(file, {
+			sourcemap: false,
+			precision: 6,           // sass中计算精度
+			// stopOnError: true,   // 错误是否忽略继续编译
+			style: "compressed",    // 压缩css
+			emitCompileError: true,
+			loadPath: ['./' + PATHS.BEFORE.CSS + 'core', './' + PATHS.BEFORE.CSS + 'module'] //查找文件根目录
+		})
+			.on('error', function (err) { // 打印日志
+				notify.onError({
+					title: 'Error!',
+					message: '<%= error.message %>',
+					sound: 'Beep'
+				})(err);
+				this.emit('end');
+			})
+			.pipe(cached('sass'))
+			.pipe(autoprefixer({
+				browsers: ['ios >= 6', 'android >= 4.0']
+			}))
+			.pipe(sourcemaps.write())
+			.pipe(header(PKG.banner, { pkg: PKG }))
+			.pipe(size({ title: 'styles', gzip: true }))
+			.pipe(gulp.dest(PATHS.AFTER.CSS))
+			.pipe(browserSync.stream())
+			.pipe(notify({ // 编译完成
+				title: 'SASS',
+				subtitle: 'success!',
+				message: 'Sass task complete！'
+			}));
+	}
+	var _watch = 'no';
+
+	if (_watch == 'watch') {
+		return compile(PATHS.BEFORE.CSS + '**/**/*.scss');
+	} else {
+		prompt.start();
+		var sassStream = null;
+		prompt.get([{
+			name: 'filename'
+		}], function (err, result) {
+			if (result.filename) {
+				sassStream = compile(PATHS.BEFORE.CSS + '**/**/' + result.filename);
+			} else {
+				sassStream = compile(PATHS.BEFORE.CSS + '**/**/*.scss');
+			}
+		});
+		// if(sassStream){
+		return sassStream;
+		// }
+	}
+
 };
